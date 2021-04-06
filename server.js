d0052288b1e8e155c253b7891b177daf8a0d1502
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-04-06 13:59:36 -0600

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //state that we are using express in this file
 //note that we don't have to give it a file path even though it is not a core module because it has been installed into node modules
 const express= require("express")
+const morgan= require("morgan")
+
 
 const hostname="localhost"
 const port = 3000
@@ -8,11 +10,19 @@ const port = 3000
 //express() returns a express server application that is now accessible under the variable name
 const app = express()
 
+//morgan is middleware used to log activity, the dev argument tells morgan function to use the development version
+//this will print additional information to the screen.
+app.use(morgan("dev"))
+
+//express.static will serve files from the public folder __dirname is a special variable in node
+// it will refer to the absolute path in the current directory of the file that it is in.
+app.use(express.static(__dirname + "/public"))
+
+
 //use method can accept a callback function express refers to this as a middleware function.
 //middleware functions have access to 3 params, req, res, and next which is a function. 
 app.use((req,res)=> 
 {
-    console.log(req.headers);
     res.statusCode = 200
     res.setHeader("Content-Type", "text/html")
     res.end("<html><body><h1>This is an Express Server</h1></body></html>")
