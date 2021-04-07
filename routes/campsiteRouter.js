dd187f02b3e10a59e562b2e7932a1735ce7363c4
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-04-07 12:10:19 -0600

    Express Router

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const campsiteRouter = express.Router()
+
+campsiteRouter.route("/")
+
+//* ENDPOINTS
+//.all is a catch all for all http verbs use this to set default props for all routing methods
+  .all((req,res,next)=> 
+{
+    res.statusCode= 200
+    //indicates we are going to send plain text in the response body
+    res.header("Content-Type", "text/plain")
+    //sends to the next relevant routing method, if not included would just stop here
+    next()
+})
+
+.get((req,res)=>{
+    res.end("Will send all the campsites to you")
+})
+
+.post((req,res)=>
+{
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`)
+})
+
+.put((req,res)=>{
+    res.statusCode = 403; 
+    res.end("PUT operation not supported on /campsites")
+})
+
+.delete((req,res)=>{
+    res.end("Deleting all campsites")
+})
+
+module.exports = campsiteRouter
